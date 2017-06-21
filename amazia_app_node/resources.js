83f1afae11af940c8a1f30b06555211454431c5b
Julia Kim <jkim0620@gmail.com>
2017-06-21 10:29:42 -0400

    Initial setup completed

---
+++ amazia_app_node/resources.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+// Uncomment these once we set up our resources folder
+
+// router.use('/users', require('./resources/users'));
+
+// router.use('/restaurants', require('./resources/restaurants'));
+
+module.exports = router;
