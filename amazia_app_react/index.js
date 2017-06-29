624d6e70568ba327e0ad304e9ce7ac7d40d58594
Julia Kim <jkim0620@gmail.com>
2017-06-29 16:13:46 -0400

    Edit route

--- amazia_app_react/index.js
+++ amazia_app_react/index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.static(__dirname + "/dist"));
 
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
     res.sendFile(__dirname + "/dist/index.html");
 });
 
